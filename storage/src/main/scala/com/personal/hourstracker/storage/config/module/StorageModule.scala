6a5d231013f9657135f917d8454f20d212bd69c6
Hans Drost <hans.drost@persgroep.net>
2019-11-29 13:18:23 +0100

    [] Enabled settings for Heroku

--- storage/src/main/scala/com/personal/hourstracker/storage/config/module/StorageModule.scala
+++ storage/src/main/scala/com/personal/hourstracker/storage/config/module/StorageModule.scala
@@ -2,10 +2,10 @@ package com.personal.hourstracker.storage.config.module
 
 import com.personal.hourstracker.config.component.LoggingComponent
 import com.personal.hourstracker.storage.config.StorageConfiguration
-import com.personal.hourstracker.storage.config.component.DefaultRegistrationRepositoryComponent
+import com.personal.hourstracker.storage.config.component.HerokuRegistrationRepositoryComponent
 import org.slf4j.{ Logger, LoggerFactory }
 
-trait StorageModule extends DefaultRegistrationRepositoryComponent with StorageConfiguration with LoggingComponent {
+trait StorageModule extends HerokuRegistrationRepositoryComponent with StorageConfiguration with LoggingComponent {
   override implicit lazy val logger: Logger = LoggerFactory.getLogger("StorageModule")
 
   logger.info(s"Using url   : ${Storage.Registrations.url}")
